a7d60cc3beafa49b5e4b4128321695d2a9f24790
Gerald Tee <93geraldtee@gmail.com>
2019-09-18 23:01:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,8 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   if hash[key]
-  return hash[key]
+    return hash[key]
+  end
 end
 
 def update_counting_hash(hash, key)
